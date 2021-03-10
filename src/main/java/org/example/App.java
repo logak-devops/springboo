fa33d647c9df48726661b84b9b82fd5fee7eac0f
logambigaik <54719289+logambigaik@users.noreply.github.com>
2021-03-10 23:16:29 +0530

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,24 @@
-package org.example;
+package hello;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
-/**
- * Hello world!
- *
- */
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class,args);
-        //System.out.println( "Hello World!" );
+public class Application {
+    
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
+
 }
